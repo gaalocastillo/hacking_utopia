8584f616d6a2864f51cdf18936513b2b91fba197
soccerdroid <mguarand@espol.edu.ec>
2017-11-25 07:37:12 -0500

    nueva forma de normalizar checkins

--- scripts/foursquare/pois_scores.py
+++ scripts/foursquare/pois_scores.py
@@ -10,7 +10,9 @@ from FileWorker import FileWorker
 fileworker = FileWorker()
 
 filename = './outputs/buildings_pois.json'
-output_fn='./outputs/buildings_scores.csv'
+#output_fn='./outputs/buildings_scores2.csv'
+
+output_fn='./outputs/buildings_scores2.csv'
 
 output=open(output_fn,'w')
 infoEdificios = fileworker.readJSON(filename)
@@ -26,30 +28,35 @@ for idEdificio in data:
   num_venues=len(list(dict_venues.keys()))
   total_checkins=0
   if(num_venues==0):
+    line=idEdificio+",0,0"
+    output.write(line+"\n")
     continue
   for id_venue in dict_venues:
     total_checkins+=int(dict_venues[id_venue]['checkins'])
   print("TOTAL CHECKINS: "+str(total_checkins))
-  for id_venue in dict_venues:
-    print(dict_venues[id_venue]['checkins'])
-    d[id_venue]=int(dict_venues[id_venue]['checkins'])/float(total_checkins)
+  score=total_checkins/float(num_venues)
+  # for id_venue in dict_venues:
+  #   print(dict_venues[id_venue]['checkins'])
+  #   d[id_venue]=int(dict_venues[id_venue]['checkins'])/float(total_checkins)
 
-  scoreList=list(d.values())
+  # scoreList=list(d.values())
  
-  scoreList.sort()
-  print(scoreList)
-  score=0
+  # scoreList.sort()
+  # print(scoreList)
+  # score=0
   
-  if(num_venues%2==0):
-    s1=scoreList[num_venues/2]
-    s2=scoreList[(num_venues/2)+1]
-    score=(s1+s2)/2
-  else:
-    score=scoreList[(num_venues+1)/2]
-  print("ASDFasd")
-  print(str(num_venues))
-  print(str(score))
-  line=idEdificio+","+str(num_venues)+","+str(score*1000)
+  # if(num_venues%2==0):
+  #   s1=scoreList[num_venues/2]
+  #   s2=scoreList[(num_venues/2)+1]
+  #   score=(s1+s2)/2
+  # else:
+  #   score=scoreList[(num_venues+1)/2]
+  # print("ASDFasd")
+  # print(str(num_venues))
+  # print(str(score))
+  # line=idEdificio+","+str(num_venues)+","+str(score*1000)
+  line=idEdificio+","+str(num_venues)+","+str(score)
+
   print (line)
   output.write(line+"\n")
 
