b7bd667716f3fa005f03e38b23b036ae6ce3551c
soccerdroid <mguarand@espol.edu.ec>
2017-11-25 04:28:10 -0500

    generados scores de pois

--- scripts/foursquare/pois_scores.py
+++ scripts/foursquare/pois_scores.py
@@ -1,9 +1,7 @@
 #!/usr/local/bin/python
 # coding: latin-1-
-import io
-import urllib2
+
 import json
-import unicodedata
 from FileWorker import FileWorker
 
 
@@ -16,7 +14,7 @@ output_fn='./outputs/buildings_scores.csv'
 
 output=open(output_fn,'w')
 infoEdificios = fileworker.readJSON(filename)
-
+output.write("id,num_venues,scores"+"\n")
 
 data = infoEdificios['data']
 coordenadas = {}
@@ -24,23 +22,34 @@ coordenadas = {}
 for idEdificio in data:
   d={}
   dict_venues=data[idEdificio]['venues']
+  print(dict_venues)
   num_venues=len(list(dict_venues.keys()))
   total_checkins=0
+  if(num_venues==0):
+    continue
   for id_venue in dict_venues:
-    total_checkins+=dict_venues[id_venue]['checkins']
+    total_checkins+=int(dict_venues[id_venue]['checkins'])
+  print("TOTAL CHECKINS: "+str(total_checkins))
   for id_venue in dict_venues:
-    d[id_venue]=dict_venues[id_venue]['checkins']/total_checkins
+    print(dict_venues[id_venue]['checkins'])
+    d[id_venue]=int(dict_venues[id_venue]['checkins'])/float(total_checkins)
 
-  scoreList=list(dict_venues.values())
-  scoreList=sort(scoreList)
+  scoreList=list(d.values())
+ 
+  #scoreList=scoreList.sort()
+  print(scoreList)
   score=0
-  tam=len(scoreList)
-  if(tam%2==0):
-    s1=scoreList[tam/2]
-    s2=scoreList[(tam/2)+1]
+  
+  if(num_venues%2==0):
+    s1=scoreList[num_venues/2]
+    s2=scoreList[(num_venues/2)+1]
     score=(s1+s2)/2
   else:
-    score=scoreList[(tam+1)/2]
-  line=data[idEdificio]⁺","+num_venues+","+score
-  output.write(line)
+    score=scoreList[(num_venues+1)/2]
+  print("ASDFasd")
+  print(str(num_venues))
+  print(str(score))
+  line=idEdificio+","+str(num_venues)+","+str(score)
+  print (line)
+  output.write(line+"\n")
 
